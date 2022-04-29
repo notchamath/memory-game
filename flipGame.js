712b1274e6100a933fea31867cffaeb27878aa08
Chamath Jayarathne <chamathsocial@gmail.com>
2022-04-29 17:06:01 -0400

    successfully getting a randomly unique pic for each card. Next task: hide all other cards when one is clicked

--- flipGame.js
+++ flipGame.js
@@ -3,48 +3,68 @@ const getCards = () => {
     return document.querySelectorAll('.cards');
 }
 
-const getPics = () => {
+//returns a number between 1 and num, bounds included
+const randomizer = (num) => {
+    return Math.floor(Math.random()*num) + 1
+}
+
+const getRandom = (numOfPics, numOfDivs) => {
+
+    let arr = [];
+    let rand1;
+    let rand2;
+
+    while(arr.length < numOfDivs){
+
+        let num = randomizer(numOfPics);
+
+        if(arr.indexOf(num) === -1) arr.push(num);
+    }
+
+    do{
+        rand1 = randomizer(arr.length)-1;
+        rand2 = randomizer(arr.length)-1;
+    } while (rand1 === rand2);
+
+    let duplicate = arr[rand1]
+    arr[rand2] = duplicate;
+
+    return arr;
+}
+
+const getPics = (numOfDivs) => {
 
     let pics = [];
     let numOfPics = 10; // Pics folder has 10 pics
 
-    for(let pic=0; pic<numOfPics; pic++){
-        pics[pic] = `url('Pics/${pic+1}.jpg')`;
+    let nums = getRandom(numOfPics, numOfDivs);
+
+    for(let pic=0; pic<numOfDivs; pic++){
+        pics[pic] = `url('Pics/${nums[pic]}.jpg')`
     }
 
     return pics;
 } 
 
-const getRandom = (len) => {
-
-    let num = Math.floor(Math.random()*len)
-
-    return num;
-}
-
 const populateCards = (divs, pics) => {
 
     for (let div=0; div<divs.length; div++){
 
-        let rand = getRandom(divs.length);
+        divs[div].addEventListener('mousedown', e => {
 
-        divs[div].addEventListener('mousedown', e =>{
+            divs[div].style.backgroundImage = pics[div];
+            divs[div].style.backgroundSize = 'cover';
 
-            console.log(rand)
-            console.log(pics)
             console.log(divs[div])
-
-            divs[div].style.backgroundImage = pics[rand];
-            divs[div].style.backgroundSize = "cover";
         });
+    }
 
-    } 
 }
 
 const initGame = () => {
     
     let divs = getCards();
-    let pics = getPics();
+    let pics = getPics(divs.length);
     populateCards(divs, pics);
     
 }
